750a23fb2cfcc53190ddd95fda3fae0041873e85
zx <tanzhuoxuan@gmail.com>
2025-05-15 18:44:20 +0800

    changed the database to the new ver

--- NomNoms/NomNoms/settings.py
+++ NomNoms/NomNoms/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'nomnoms_db',
         'USER': 'root',
         'PASSWORD': 'wearenumberone',
-        'HOST': 'localhost', 
+        'HOST': '127.0.0.1', 
         'PORT': '3306'
     }
 }
